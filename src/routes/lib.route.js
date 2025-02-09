4c6f2df25bffd3373cdeb3dee5a29b9c8d79edf0
Harshit <harshit2001pandey@gmail.com>
2025-02-09 14:32:50 +0530

    Book Routes Completed

--- src/routes/lib.route.js
+++ src/routes/lib.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { addBook, deleteBook, getBookById, getBooks, updateBook } from '../controller/book.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
-import Book from '../models/book.model.js';
 const route = express();
 route.post('/upload', authMiddleware, addBook);
 route.get('/books', getBooks);
